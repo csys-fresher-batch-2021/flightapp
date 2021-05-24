864b234fc59f430ef25ac94bb0e88d27570c78f8
kavinkavin16 <47984191+kavinkavin16@users.noreply.github.com>
2021-05-24 20:00:05 +0530

    updates exception feature

--- Flight_checking.sql
+++ Flight_checking.sql
@@ -14,7 +14,7 @@ IS CURSOR CUR1 IS
   f_fare    	Flights_Team1.ticket_cost%TYPE;
   f_num_of_seat Flights_Team1.AVAILABLE_SEATS%TYPE;
   f_name        Flights_Team1.FLIGHT_NAME%TYPE;
---  no_fli EXCEPTION;
+ no_fli EXCEPTION;
 BEGIN  
         OPEN CUR1;
         LOOP
@@ -22,15 +22,15 @@ BEGIN
         EXIT WHEN CUR1%NOTFOUND;
          DBMS_OUTPUT.PUT_LINE('flight id: ' || f_fnumber || ' From:  ' || f_source || ' Destination: ' || f_destination || ' Date_On_Journey: ' || f_date || ' Ticket_Cost ' || f_fare || ' Flight_Name ' || f_name);
         END LOOP;
---         IF CUR1%NOTFOUND THEN
---            RAISE no_fli;
---         END IF;
+        IF CUR1%NOTFOUND THEN
+           RAISE no_fli;
+        END IF;
     CLOSE CUR1;
---    EXCEPTION 
---        WHEN no_fli THEN 
---            DBMS_OUTPUT.PUT_LINE('No Flights matched');
+   EXCEPTION 
+       WHEN no_fli THEN 
+           DBMS_OUTPUT.PUT_LINE('No Flights matched');
 END;
 desc Flights_Team1;
 select * from Flights_team1;
-execute VIEWFL('Hydeabad','Kolkata','02-JUN-21 12.00.00 PM',3);
+execute VIEWFL('Hyderabad','Kolkata','02-JUN-21 12.00.00 PM',3);
 commit;
